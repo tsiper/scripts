9b55e1d87abe3c25fb45bbf66021ef103d339ff9
tsiper <tsiper@gmail.com>
2018-03-05 10:17:26 +0200

    updated the dotfiles script

---
+++ dotfiles.sh
@@ -0,0 +1,17 @@
+cd
+# Cloning my scripts directory
+git clone https://github.com/tsiper/scripts.git
+git clone --bare https://github.com/tsiper/dotfiles.git $HOME/.dotfiles
+function dotfiles {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
