c417721d5c89726b83950e13b2cd9d9b091400bc
tsiper <tsiper@gmail.com>
2018-03-05 10:46:52 +0200

    cleanup

--- install_vim.sh
+++
@@ -1,7 +0,0 @@
-cd
-mv .vim .vim_backup
-# Required for vim plugins
-sudo apt install cmake
-mkdir -p .vim/autoload
-wget -O .vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
